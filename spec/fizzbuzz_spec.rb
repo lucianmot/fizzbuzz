5ed39c30883a5907e0a68e3549bceafbf80db7f0
PunchCafe <lfruzza@hotmail.co.uk>
2018-12-12 16:22:26 +0000

    2nd Red light pass fizz if multiple of 3

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,8 @@ describe 'fizzbuzz' do
 	it 'returns given_number when passed 4' do
 		expect(fizzbuzz(4)).to eq 4
 	end
+	it 'returns fizz when passed a multiple of 3' do
+		expect(fizzbuzz(3)).to eq 'fizz'
+		expect(fizzbuzz(6)).to eq 'fizz'
+	end
 end
