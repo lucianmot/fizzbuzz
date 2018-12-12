be0efcf76f43f34d862c2077f83a3214553b5cc0
PunchCafe <lfruzza@hotmail.co.uk>
2018-12-12 16:31:46 +0000

    3rd red light pass multiple of 5 get buzz

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,4 +8,8 @@ describe 'fizzbuzz' do
 		expect(fizzbuzz(3)).to eq 'fizz'
 		expect(fizzbuzz(6)).to eq 'fizz'
 	end
+	it 'returns buzz when passed a multiple of 5' do
+		expect(fizzbuzz(5)).to eq 'buzz'
+		expect(fizzbuzz(10)).to eq 'buzz'
+	end
 end
