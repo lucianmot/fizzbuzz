3e87527c4eeb0d7b50f3e3c219de1b143e2eb8bb
PunchCafe <lfruzza@hotmail.co.uk>
2018-12-12 16:34:57 +0000

    refactored

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,8 +1,5 @@
 def fizzbuzz(number)
-  if number % 3 == 0
-  	return 'fizz'
-  elsif number %5 == 0
-  	return 'buzz'
-  end
+  return 'fizz' if number % 3 == 0
+  return 'buzz' if number %5 == 0
   return number
 end
