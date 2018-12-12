9a69efe9fac2a959e16961c580d0d263da3764f5
PunchCafe <lfruzza@hotmail.co.uk>
2018-12-12 17:15:53 +0000

    R2: 3rd red light give multiple of 3 and 5 expect 'fizzbuzz'

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,6 +1,11 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'if given multipled of 3 and 5 returns "fizzbuzz"'do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    expect(fizzbuzz(45)).to eq 'fizzbuzz'
+  end
   it 'given multiple of 3 returns "fizz"' do
     expect(fizzbuzz(3)).to eq 'fizz'
     expect(fizzbuzz(6)).to eq 'fizz'
