54b611ca5ae87d76c627afca808f8566087d06bd
PunchCafe <lfruzza@hotmail.co.uk>
2018-12-12 17:08:13 +0000

    r2: Second red light, multiples of 3,5 fizz buzz respectively.

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,14 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'given 3 returns "fizz"' do
+  it 'given multiple of 3 returns "fizz"' do
     expect(fizzbuzz(3)).to eq 'fizz'
+    expect(fizzbuzz(6)).to eq 'fizz'
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+  it 'given multiple of 5 return "buzz"' do
+  	expect(fizzbuzz(5)).to eq 'buzz'
+  	expect(fizzbuzz(10)).to eq 'buzz'
+  	expect(fizzbuzz(20)).to eq 'buzz'
   end
 end
